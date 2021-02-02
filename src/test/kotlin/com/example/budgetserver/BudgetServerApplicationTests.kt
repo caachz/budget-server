1f9dc746a1ce1958a3812da7bf0dd380655b52c3
Cassie Achzenick <cassieachzenick@Cassies-MacBook-Pro.local>
2021-02-01 18:02:58 -0700

    Initial commit

---
+++ src/test/kotlin/com/example/budgetserver/BudgetServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.budgetserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
